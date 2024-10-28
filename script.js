f645472753571433dcc0c6168d9cbb1a6f4be1ed
Jignesh Vaghela <72225073+jigar7591@users.noreply.github.com>
2024-10-28 15:04:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,32 +6,4 @@ let charIndex = 0;
 
 function type() {
     if (charIndex < textArray[textIndex].length) {
-        typingText.textContent += textArray[textIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, 100);
-    } else {
-        setTimeout(erase, 2000);
-    }
-}
-
-function erase() {
-    if (charIndex > 0) {
-        typingText.textContent = textArray[textIndex].substring(0, charIndex - 1);
-        charIndex--;
-        setTimeout(erase, 50);
-    } else {
-        textIndex = (textIndex + 1) % textArray.length;
-        setTimeout(type, 500);
-    }
-}
-
-document.addEventListener("DOMContentLoaded", type);
-
-// Modal Functionality
-function openModal(projectId) {
-    document.getElementById(`${projectId}-modal`).style.display = "flex";
-}
-
-function closeModal(projectId) {
-    document.getElementById(`${projectId}-modal`).style.display = "none";
-}
+        typingText.textContent += textArray[textIndex].charAt(char
